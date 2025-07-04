95c00e50a5b70b0533c5fb9f36efbe797fe38168
anakhapdileepkumar <anakhapdileep20@gmail.com>
2025-07-04 22:19:08 +0530

    fourth commit

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     int num1,num2,sum;
-    printf("Enter two numbers");
+    printf("Enter two numbers:");
     scanf("%d%d",&num1,&num2);
     sum=num1+num2;
     printf("Sum is %d",sum);
